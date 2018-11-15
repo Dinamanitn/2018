e03545f81ff4eb4746b6ed4245c37cff2c609d71
Dinamanitn <37095127+Dinamanitn@users.noreply.github.com>
2018-11-15 10:47:18 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo yum install tomcat -y
+sudo yum install tomcat -y --skip-broken
 sudo sed -i 's/port="8080"/port="8880"/' /etc/tomcat/server.xml
-sudo yum install tomcat-webapps tomcat-admin-webapps -y
-sudo yum install tomcat-docs-webapp tomcat-javadoc -y
+sudo yum install tomcat-webapps tomcat-admin-webapps -y --skip-broken
+sudo yum install tomcat-docs-webapp tomcat-javadoc -y --skip-broken
